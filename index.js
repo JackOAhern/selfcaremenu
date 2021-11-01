00b9b30b11609ee9a749e04e2cfaf99b42cc9152
JackOAhern <68722351+JackOAhern@users.noreply.github.com>
2021-10-31 21:13:47 -0600

    Replaced "time" with "quotes"

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-// Assigning all necessary data for each day (button) with key value pairs
+// Assigning all necessary data for each day (button) with key value pairs. Functions pull from these datasets for each day of the week.
+// Figured this would be the best way for me to organize and work through all of my different data.
+// citations: https://javascript.info/object
+// https://www.w3schools.com/cssref/sel_active.asp
+// https://stackoverflow.com/questions/31178653/how-to-keep-active-css-style-after-click-a-button
+// https://www.w3schools.com/jsref/prop_element_children.asp
 
 let data = {
   monday: {
@@ -7,7 +12,7 @@ let data = {
       dateinfo: {
         dayoweek: "monday",
         date: "11/01/2021",
-        time: "9:30",
+        quote: "Dream big and dare to fail.",
         temp: "57°",
         weather: "cloudy"
       }
@@ -18,29 +23,29 @@ let data = {
         dateinfo: {
             dayoweek: "tuesday",
             date: "11/02/2021",
-            time: "9:30",
+            quote: "Stay foolish to stay sane.",
             temp: "48°",
             weather: "partly sunny"
         }
   },
   wednesday: {
-        todos: ["Breakfast","Leg-Day Workout","Post-Workout Shake","Shower & Lunch","Data Science Class","Writing Homework", "Design Homework","Stretch for 10min"],
+        todos: ["Breakfast","Leg-Day Workout","Post-Workout Shake","Shower & Lunch","Data Science Class","Writing Homework", "Design Homework","Stretch for 10 min"],
         image: "./images/wednesdaydog.jpg",
         dateinfo: {
             dayoweek: "wednesday",
             date: "11/03/2021",
-            time: "9:30",
+            quote: "you are enough just as you are.",
             temp: "52°",
             weather: "cloudy"
         }
   },
   thursday: {
-        todos: ["Breakfast","Shower","Writing Class","Design Class", "Push-Day Workout", "Post-Workout Shake","Coding Homework","Stretch for 5 min"],
+        todos: ["Breakfast","Shower","Writing Class","Design Class", "Push-Day Workout", "Post-Workout Shake","Coding Homework","Job Applications"],
         image: "./images/thursdaydog.jpg",
         dateinfo: {
             dayoweek: "Thursday",
             date: "11/04/2021",
-            time: "9:30",
+            quote: "Keep going. Be All In.",
             temp: "54°",
             weather: "cloudy"
         }
@@ -51,18 +56,18 @@ let data = {
         dateinfo: {
             dayoweek: "Friday",
             date: "11/05/2021",
-            time: "9:30",
+            quote: "Leave no stone unturned.",
             temp: "62°",
             weather: "sunny"
         }
   },
   saturday: {
-        todos: ["Breakfast","Skiing @ArapahoeBasin", "Shower & Dinner","Hanging with friends", "Stretch for 10min"],
+        todos: ["Breakfast","Skiing @Arapahoe_Basin", "Shower & Dinner","Hanging with friends", "Stretch for 10 min"],
         image: "./images/saturdaydog.jpg",
         dateinfo: {
             dayoweek: "Saturday",
             date: "11/06/2021",
-            time: "9:30",
+            quote: "Good things happen to those who hustle.",
             temp: "65°",
             weather: "sunny"
         }
@@ -73,7 +78,7 @@ let data = {
         dateinfo: {
             dayoweek: "Sunday",
             date: "11/07/2021",
-            time: "9:30",
+            quote: "You never fail until you stop trying.",
             temp: "51°",
             weather: "Rainy",
         }
@@ -84,19 +89,19 @@ let data = {
 document.getElementById("monday").addEventListener("click", function infoChange(event){
 
     document.getElementById("day-o-week").innerHTML = data.monday.dateinfo.dayoweek;
-    document.getElementById("current-date").innerHTML =
-      data.monday.dateinfo.date;
-    document.getElementById("time").innerHTML = data.monday.dateinfo.time;
+    document.getElementById("current-date").innerHTML = data.monday.dateinfo.date;
+    document.getElementById("quote").innerHTML = data.monday.dateinfo.quote;
     document.getElementById("temp").innerHTML = data.monday.dateinfo.temp;
     document.getElementById("weather").innerHTML = data.monday.dateinfo.weather;
     document.getElementById("image").src = data.monday.image;
-    document.getElementById("list").innerHTML = "";
+    document.getElementById("list").innerHTML = ""; //when we click a new button,
+    // I figured i had to first clear the old list to be able to append a new one
 
       for (let i of data.monday.todos){
           let li = document.createElement("li");
           li.innerHTML = i;
           document.getElementById("list").appendChild(li);
-          let buttonsChildren = document.getElementById("buttons").children;
+          let buttonsChildren = document.getElementById("buttons").children; //The following code checks to see if any other buttons are currently active
           for (let i = 0; i < buttonsChildren.length; i++) {
               let button = buttonsChildren.item(i);
               if (button.classList.contains("active") && event.target != button){
@@ -112,9 +117,8 @@ document.getElementById("monday").addEventListener("click", function infoChange(
 document.getElementById("tuesday").addEventListener("click", function infoChange(event) {
 
     document.getElementById("day-o-week").innerHTML = data.tuesday.dateinfo.dayoweek;
-    document.getElementById("current-date").innerHTML =
-      data.tuesday.dateinfo.date;
-    document.getElementById("time").innerHTML = data.tuesday.dateinfo.time;
+    document.getElementById("current-date").innerHTML = data.tuesday.dateinfo.date;
+    document.getElementById("quote").innerHTML = data.tuesday.dateinfo.quote;
     document.getElementById("temp").innerHTML = data.tuesday.dateinfo.temp;
     document.getElementById("weather").innerHTML = data.tuesday.dateinfo.weather;
     document.getElementById("image").src = data.tuesday.image;
@@ -140,9 +144,8 @@ document.getElementById("tuesday").addEventListener("click", function infoChange
 document.getElementById("wednesday").addEventListener("click", function infoChange(event){
 
     document.getElementById("day-o-week").innerHTML = data.wednesday.dateinfo.dayoweek;
-    document.getElementById("current-date").innerHTML =
-      data.wednesday.dateinfo.date;
-    document.getElementById("time").innerHTML = data.wednesday.dateinfo.time;
+    document.getElementById("current-date").innerHTML = data.wednesday.dateinfo.date;
+    document.getElementById("quote").innerHTML = data.wednesday.dateinfo.quote;
     document.getElementById("temp").innerHTML = data.wednesday.dateinfo.temp;
     document.getElementById("weather").innerHTML = data.wednesday.dateinfo.weather;
     document.getElementById("image").src = data.wednesday.image;
@@ -168,9 +171,8 @@ document.getElementById("wednesday").addEventListener("click", function infoChan
 document.getElementById("thursday").addEventListener("click", function infoChange(event){
 
     document.getElementById("day-o-week").innerHTML = data.thursday.dateinfo.dayoweek;
-    document.getElementById("current-date").innerHTML =
-      data.wednesday.dateinfo.date;
-    document.getElementById("time").innerHTML = data.thursday.dateinfo.time;
+    document.getElementById("current-date").innerHTML = data.wednesday.dateinfo.date;
+    document.getElementById("quote").innerHTML = data.thursday.dateinfo.quote;
     document.getElementById("temp").innerHTML = data.thursday.dateinfo.temp;
     document.getElementById("weather").innerHTML = data.thursday.dateinfo.weather;
     document.getElementById("image").src = data.thursday.image;
@@ -196,9 +198,8 @@ document.getElementById("thursday").addEventListener("click", function infoChang
 document.getElementById("friday").addEventListener("click", function infoChange(event){
 
     document.getElementById("day-o-week").innerHTML = data.friday.dateinfo.dayoweek;
-    document.getElementById("current-date").innerHTML =
-      data.wednesday.dateinfo.date;
-    document.getElementById("time").innerHTML = data.friday.dateinfo.time;
+    document.getElementById("current-date").innerHTML = data.wednesday.dateinfo.date;
+    document.getElementById("quote").innerHTML = data.friday.dateinfo.quote;
     document.getElementById("temp").innerHTML = data.friday.dateinfo.temp;
     document.getElementById("weather").innerHTML = data.friday.dateinfo.weather;
     document.getElementById("image").src = data.friday.image;
@@ -224,9 +225,8 @@ document.getElementById("friday").addEventListener("click", function infoChange(
 document.getElementById("saturday").addEventListener("click", function infoChange(event){
 
     document.getElementById("day-o-week").innerHTML = data.saturday.dateinfo.dayoweek;
-    document.getElementById("current-date").innerHTML =
-      data.wednesday.dateinfo.date;
-    document.getElementById("time").innerHTML = data.saturday.dateinfo.time;
+    document.getElementById("current-date").innerHTML = data.wednesday.dateinfo.date;
+    document.getElementById("quote").innerHTML = data.saturday.dateinfo.quote;
     document.getElementById("temp").innerHTML = data.saturday.dateinfo.temp;
     document.getElementById("weather").innerHTML = data.saturday.dateinfo.weather;
     document.getElementById("image").src = data.saturday.image;
@@ -252,9 +252,8 @@ document.getElementById("saturday").addEventListener("click", function infoChang
 document.getElementById("sunday").addEventListener("click", function infoChange(event){
 
     document.getElementById("day-o-week").innerHTML = data.sunday.dateinfo.dayoweek;
-    document.getElementById("current-date").innerHTML =
-      data.wednesday.dateinfo.date;
-    document.getElementById("time").innerHTML = data.sunday.dateinfo.time;
+    document.getElementById("current-date").innerHTML = data.wednesday.dateinfo.date;
+    document.getElementById("quote").innerHTML = data.sunday.dateinfo.quote;
     document.getElementById("temp").innerHTML = data.sunday.dateinfo.temp;
     document.getElementById("weather").innerHTML = data.sunday.dateinfo.weather;
     document.getElementById("image").src = data.sunday.image;
